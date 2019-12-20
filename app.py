5188696b59d140a6c38177f7ec1755a62b4ab135
chiraghs <chiraghs1929@gmail.com>
2019-12-20 14:37:58 +0530

    First_commit

---
+++ app.py
@@ -0,0 +1,25 @@
+#hey this is a comment
+print("hello World!")
+
+#variables
+f_name="Chirag"
+age=20
+float_numb=3.14
+l_name="H S"
+
+#String Concatenation
+name=f_name +l_name
+print(name)
+
+
+#Use f " sentence {Variables} sentence " 
+greet= f"hi there {f_name} world "
+print(greet)
+
+#To print "" use backslash
+print("hello world \"INDIA\" hi world")
+
+#multiple lines
+print("""Hey there first line
+              second line
+third line""")
