c252066027ff0d4378d1fa79bb162473c1cf72d3
chiraghs <chiraghs1929@gmail.com>
2019-12-20 14:50:02 +0530

    strings length & some operations

---
+++ strings.py
@@ -0,0 +1,18 @@
+#string length
+
+language="python"
+
+print(len(language))
+
+print(language[3])
+
+print(language[2:5])
+print(language[2:])
+print(language[:4])
+
+#String operations
+Got="HI Valaria AEGON LOVEd DAENERYS,his Aunt!!!!!"
+print(Got.upper())
+print(Got.lower())
+print(Got.find('LOVEd'))
+print(Got.replace('LOVEd','killed'))
