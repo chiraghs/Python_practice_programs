f757493c68155fe38608c80d6b4eb53df207674e
chiraghs <chiraghs1929@gmail.com>
2019-12-20 15:01:49 +0530

    if_else COnditional Statements

---
+++ statements.py
@@ -0,0 +1,15 @@
+age=21
+
+if age>=18:
+      print("You are adult")
+elif age >= 14:
+      print("Biological Adult")
+else:
+       print("You ar young")
+
+
+is_logged=True
+if is_logged:
+    print("WElcome")
+
+    #Falsy values are 0 , [] , "" ,False
