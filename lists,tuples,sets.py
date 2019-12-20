908d01f190d5b1f93f0d0f92b7b18c7f82237085
chiraghs <chiraghs1929@gmail.com>
2019-12-20 15:29:48 +0530

    Lists,tuples,sets- differneces

---
+++ lists,tuples,sets.py
@@ -0,0 +1,11 @@
+#can be modified
+lists=['a','b','c','d']
+print(lists)
+
+#cannot be modified ,and values can be repeated
+tuples=('a','b','b','v','c')
+print(tuples)
+
+#cannot be modified ,and values cannot be repeated
+sets={'a','b','b','v','c'}
+print(sets)
